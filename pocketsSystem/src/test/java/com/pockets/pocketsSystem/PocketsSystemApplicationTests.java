fc4ec69d2e98854f232af216d9906aa8b16b7524
jvianad <jeanvianademares@gmail.com>
2023-08-23 12:05:44 -0500

    configuracion inicial del proyecto

---
+++ pocketsSystem/src/test/java/com/pockets/pocketsSystem/PocketsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pockets.pocketsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
